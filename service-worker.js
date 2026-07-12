d3c4e0227bc6da03cf573b531a85030148f04378
salexandrafernanda <s.alexandrafernanda@gmail.com>
2026-07-12 00:10:05 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,65 @@
+const VERSAO = 'fitlife-v1';
+const SHELL = [
+'./',
+'./index.html',
+'./icon-192.png',
+'./icon-512.png',
+'./icon-maskable.png',
+'./apple-touch-icon.png',
+'./manifest.json'
+];
+const REDE_SEMPRE = ['api.anthropic.com', 'generativelanguage.googleapis.com'];
+
+self.addEventListener('install', e => {
+e.waitUntil(
+caches.open(VERSAO)
+.then(c => c.addAll(SHELL).catch(() => c.add('./index.html')))
+.then(() => self.skipWaiting())
+);
+});
+
+self.addEventListener('activate', e => {
+e.waitUntil(
+caches.keys()
+.then(ks => Promise.all(ks.filter(k => k !== VERSAO).map(k => caches.delete(k))))
+.then(() => self.clients.claim())
+);
+});
+
+self.addEventListener('fetch', e => {
+const req = e.request;
+if (req.method !== 'GET') return;
+const url = new URL(req.url);
+
+if (REDE_SEMPRE.some(d => url.hostname.includes(d))) return;
+
+if (url.hostname.includes('fonts.googleapis.com') || url.hostname.includes('fonts.gstatic.com')) {
+e.respondWith(
+caches.match(req).then(hit => hit || fetch(req).then(res => {
+const copia = res.clone();
+caches.open(VERSAO).then(c => c.put(req, copia));
+return res;
+}).catch(() => hit))
+);
+return;
+}
+
+if (url.origin === location.origin) {
+e.respondWith(
+caches.match(req).then(hit => {
+const rede = fetch(req).then(res => {
+if (res && res.status === 200) {
+const copia = res.clone();
+caches.open(VERSAO).then(c => c.put(req, copia));
+}
+return res;
+}).catch(() => hit);
+return hit || rede;
+})
+);
+}
+});
+
+self.addEventListener('message', e => {
+if (e.data === 'atualizar') self.skipWaiting();
+});
